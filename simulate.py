9d4776f435e17d81088198837d9fb4dc20c6a5bf
Evan Meade <evan.meade@protonmail.com>
2021-01-18 02:38:32 -0600

    Large trial

--- simulate.py
+++ simulate.py
@@ -8,6 +8,7 @@ n = int(sys.argv[1])
 suits = int(sys.argv[2])
 values = int(sys.argv[3])
 tie = int(sys.argv[4])
+prefix = sys.argv[5]
 
 print(f'Simulating {n} games with {suits} suits, {values} values, and {tie} unflipped cards per tie...')
 
@@ -29,7 +30,7 @@ for i in range(n):
 
 stop = time.time()
 
-war_game_data.to_csv('war_game_data.csv', index=False)
-war_turn_data.to_csv('war_turn_data.csv', index=False)
+war_game_data.to_csv(f'{prefix}_game_data.csv', index=False)
+war_turn_data.to_csv(f'{prefix}_turn_data.csv', index=False)
 
 print(f'Simulated {n} games in {stop - start} seconds')
