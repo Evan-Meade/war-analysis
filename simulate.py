8562e870044e4c9fd9906dbf7d2bca28b61e1b17
Evan Meade <evan.meade@protonmail.com>
2021-01-18 23:12:50 -0600

    Parallelized simulation

--- simulate.py
+++ simulate.py
@@ -1,36 +1,56 @@
+import os
 import sys
 import time
+import concurrent.futures
 import pandas as pd
 from war import WarGame
 
 
-n = int(sys.argv[1])
-suits = int(sys.argv[2])
-values = int(sys.argv[3])
-tie = int(sys.argv[4])
-prefix = sys.argv[5]
+NUM = int(sys.argv[1])
+SUITS = int(sys.argv[2])
+VALUES = int(sys.argv[3])
+TIE = int(sys.argv[4])
+PREFIX = sys.argv[5]
 
-print(f'Simulating {n} games with {suits} suits, {values} values, and {tie} unflipped cards per tie...')
+print(f'Simulating {NUM} games with {SUITS} suits, {VALUES} values, and {TIE} unflipped cards per tie...')
 
-wg = WarGame(suits, values, tie)
-war_game_data = pd.DataFrame()
-war_turn_data = pd.DataFrame()
+def simulate(n, s, v, t):
+    wg = WarGame(s, v, t)
+    war_game_data = pd.DataFrame()
+    war_turn_data = pd.DataFrame()
 
-start = time.time()
+    for i in range(n):
+        wg.reset()
+        wg.play_game()
 
-for i in range(n):
-    wg.reset()
-    wg.play_game()
-
-    game_data = wg.game_dict()
-    game_df = pd.DataFrame(game_data, index=[i])
-    war_game_data = pd.concat([war_game_data, game_df])
+        game_data = wg.game_dict()
+        game_df = pd.DataFrame(game_data, index=[i])
+        war_game_data = pd.concat([war_game_data, game_df])
+        
+        war_turn_data = pd.concat([war_turn_data, wg.turn_data])
     
-    war_turn_data = pd.concat([war_turn_data, wg.turn_data])
+    return war_game_data, war_turn_data
+#     return True
 
-stop = time.time()
+master_game_data = pd.DataFrame()
+master_turn_data = pd.DataFrame()
 
-war_game_data.to_csv(f'{prefix}_game_data.csv', index=False)
-war_turn_data.to_csv(f'{prefix}_turn_data.csv', index=False)
+N_CPU = os.cpu_count()
+nums = [NUM // N_CPU] * N_CPU
+for i in range(NUM % N_CPU):
+    nums[i] += 1
+params = [[nums[i], SUITS, VALUES, TIE] for i in range(N_CPU)]
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+#     results = [executor.submit(simulate, params[i]) for i in range(N_CPU)]
+#     for result in concurrent.futures.as_completed(results):
+#         print(result)
+    results = executor.map(simulate, params)
+    for result in results:
+        master_game_data = pd.concat([master_game_data, result[0]])
+        master_turn_data = pd.concat([master_turn_data, result[1]])
+
+master_game_data.to_csv(f'{prefix}_game_data.csv', index=False)
+master_turn_data.to_csv(f'{prefix}_turn_data.csv', index=False)
 
 print(f'Simulated {n} games in {stop - start} seconds')
